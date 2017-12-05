77255934617bd5110719b947ee7514d9e419ef7c
MP <duchungk7@gmail.com>
2017-12-05 11:59:02 +0800

    Add files via upload

---
+++ Polynomial_Regression/my_polynomial_regression.py
@@ -0,0 +1,98 @@
+# Data Preprocessing Template
+
+"""Step01: download data"""
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values
+
+# Fitting Linear Regression to the dataset
+from sklearn.linear_model import LinearRegression
+lin_reg = LinearRegression()
+lin_reg.fit(X, y)
+
+
+# Fitting Polynomial Regression to the dataset
+from sklearn.preprocessing import PolynomialFeatures
+poly_reg = PolynomialFeatures(degree = 10)
+X_poly = poly_reg.fit_transform(X)
+
+lin_reg_2 = LinearRegression()
+lin_reg_2.fit(X_poly, y)
+
+
+# Visualising the Linear Regression Results
+plt.scatter(X, y, color = 'red')
+plt.plot(X, lin_reg.predict(X), color = 'blue')
+plt.title('Truth or Bluff (Linear Regression)')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
+
+# Visualising the Polynomial Regression Results
+X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape(len(X_grid), 1)
+
+plt.scatter(X, y, color = 'red')
+plt.plot(X_grid, lin_reg_2.predict(poly_reg.fit_transform(X_grid)), color = 'blue')
+plt.title('Truth or Bluff (Polynomial Regression)')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
+
+
+# Predicting a new result with Linear Regression
+print(lin_reg.predict(9))
+
+# Predicting a new result with Polynomial Regression
+print(lin_reg_2.predict(poly_reg.fit_transform(9)))
+
+
+"""
+#Step02: Missing data processing 
+# Taking care of missing data
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = "NaN", strategy = "median", axis = 0)
+imputer= imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
+
+
+#Step03: Encoding data 
+# Encoding categorical
+# transfer to number
+# France  0
+# Spain   1
+# Germany 2
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
+
+# transfer to one hot
+# France  1   0   0
+# Spain   0   1   0
+# Germany 0   0   1
+onehotencoder = OneHotEncoder(categorical_features=[0])
+X = onehotencoder.fit_transform(X).toarray()
+
+# encoding y data
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y)
+
+
+#Step04: Split Training set and Test set
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+
+#Step05: Feature Scaling 
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+"""
