0a1f80061c844b24099bd752b3fe62134976f84a
MP <duchungk7@gmail.com>
2017-12-05 10:01:44 +0800

    Update my_simple_linear_regression.py

--- Linear_Regression/Python/my_simple_linear_regression.py
+++ Linear_Regression/Python/my_simple_linear_regression.py
@@ -1,6 +1,5 @@
 # Data Preprocessing Template
 
-"""Step01: download data"""
 # Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
@@ -11,11 +10,9 @@ dataset = pd.read_csv('Salary_Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 1].values
 
-#Step04: Split Training set and Test set
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
-
 
 # Fitting Simple Linear Regression to the Training Set
 from sklearn.linear_model import LinearRegression
@@ -48,7 +45,7 @@ plt.show()
 
 
 """
-#Step02: Missing data processing 
+# Missing data processing 
 # Taking care of missing data
 from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values = "NaN", strategy = "median", axis = 0)
@@ -56,7 +53,7 @@ imputer= imputer.fit(X[:, 1:3])
 X[:, 1:3] = imputer.transform(X[:, 1:3])
 
 
-#Step03: Encoding data 
+# Encoding data 
 # Encoding categorical
 # transfer to number
 # France  0
@@ -78,17 +75,15 @@ labelencoder_y = LabelEncoder()
 y = labelencoder_y.fit_transform(y)
 
 
-#Step04: Split Training set and Test set
+# Split Training set and Test set
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 
-#Step05: Feature Scaling 
+# Feature Scaling 
 from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 """
-
-"""@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ """
